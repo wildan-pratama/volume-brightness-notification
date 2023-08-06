d5113da386ba0e07bb4e26a2d176a88bb5b45fb7
Wildan Pratama <willdanpratamaa@gmail.com>
2023-08-06 10:09:00 +0700

    update

--- volume.sh
+++ volume.sh
@@ -27,31 +27,28 @@ function is_mute {
 function send_notification {
     DIR=`dirname "$0"`
     volume=`get_volume`
+    
     # Make the bar with the special character ─ (it's not dash -)
     # https://en.wikipedia.org/wiki/Box-drawing_character
-#bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
-if [ "$volume" = "0" ]; then
+	#bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
+	bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
+	
+	if [ "$volume" = "0" ]; then
         icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
-$DIR/notify-send.sh "$volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace-id=555
+		notify-send "$volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace-id=555
+    elif [  "$volume" -lt "10" ]; then
+	    icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+		notify-send "$volume""     " -i "$icon_name" --replace-id=555 -t 2000
+    elif [ "$volume" -lt "30" ]; then
+        icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+    elif [ "$volume" -lt "70" ]; then
+        icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-medium.svg"
     else
-	if [  "$volume" -lt "10" ]; then
-	     icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
-$DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace-id=555 -t 2000
-    else
-        if [ "$volume" -lt "30" ]; then
-            icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
-        else
-            if [ "$volume" -lt "70" ]; then
-                icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-medium.svg"
-            else
-                icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
-            fi
-        fi
-    fi
-fi
-bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
-# Send the notification
-notify-send "$volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace-id=555
+        icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
+	fi
+	
+	# Send the notification
+	notify-send "$volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace-id=555
 
 }
 
@@ -64,7 +61,9 @@ case $1 in
 	send_notification
 	;;
     down)
+    # Set the volume on (if it was muted)
 	amixer -D "$result" set Master on > /dev/null
+	# Down the volume (- 5%)
 	amixer -D "$result" sset Master 5%- > /dev/null
 	send_notification
 	;;
@@ -73,7 +72,7 @@ case $1 in
 	amixer -D "$result" set Master 1+ toggle > /dev/null
 	if is_mute ; then
     DIR=`dirname "$0"`
-    $DIR/notify-send.sh -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" --replace-id=555 -u normal "Mute" -t 2000
+    notify-send -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" --replace-id=555 -u normal "Mute" -t 2000
 	else
 	    send_notification
 	fi
